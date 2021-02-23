ae9db3fcded811008519550448503ac04859b6be
Helmut josue colindres brenes <helmut-1521@hotmail.com>
2021-02-23 15:56:41 -0600

    Agregando Boton para Compartir

--- app/src/main/java/com/helcode/catalogo_sinevol/MainDetalleProducto.java
+++ app/src/main/java/com/helcode/catalogo_sinevol/MainDetalleProducto.java
@@ -1,8 +1,14 @@
 package com.helcode.catalogo_sinevol;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -38,5 +44,22 @@ itemList itemDatail;
         NombreDetalle.setText(itemDatail.getNombre());
         Descripcion.setText(itemDatail.getDescripcion());
         Precio.setText(String.valueOf( itemDatail.getPrecio()));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_shared,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id= item.getItemId();
+        if (id==R.id.share){
+            ApplicationInfo api = getApplicationContext().getApplicationInfo();
+            String apkpath=api.sourceDir;
+            Intent intent= new Intent(Intent.ACTION_SEND);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
