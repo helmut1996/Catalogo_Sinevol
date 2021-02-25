9cb110208f7b690751492bcabfc7f9b7e76da6e3
Helmut josue colindres brenes <helmut-1521@hotmail.com>
2021-02-25 16:04:13 -0600

    ConexionSQLiteHelper

--- app/src/main/java/com/helcode/catalogo_sinevol/MainActivity.java
+++ app/src/main/java/com/helcode/catalogo_sinevol/MainActivity.java
@@ -3,30 +3,58 @@ package com.helcode.catalogo_sinevol;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.helcode.catalogo_sinevol.SQLite.ConexionSQLiteHelper;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
-EditText user,pass;
+EditText passw;
+Spinner user;
 Button login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         user=findViewById(R.id.textUser);
-        pass=findViewById(R.id.textpass);
+        passw=findViewById(R.id.textpass);
         login=findViewById(R.id.btn_Login);
-
         login.setOnClickListener(this);
-    }
+
+
+        ArrayAdapter<CharSequence> adapter1 =ArrayAdapter.createFromResource(this, R.array.User, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        user.setAdapter(adapter1);
+
+            }
 
     @Override
     public void onClick(View view) {
-        Intent intent = new Intent(getApplicationContext(),MainProductos.class);
-        startActivity(intent);
 
+        switch (view.getId()){
+
+            case R.id.btn_Login:
+                Intent intent =new Intent(getApplicationContext(),MainProductos.class);
+                startActivity(intent);
+                break;
+
+            }
+
+       }
     }
-}
+
+
+
+
--- app/src/main/java/com/helcode/catalogo_sinevol/MainEmail.java
+++ app/src/main/java/com/helcode/catalogo_sinevol/MainEmail.java
@@ -3,12 +3,32 @@ package com.helcode.catalogo_sinevol;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainEmail extends AppCompatActivity {
+
+    EditText correo,nombreUsuario,mensaje;
+    ImageView imagen_MSM;
+    Button btnEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_email);
+
+        setTitle("Enviar Correo");
+
+
+        initViewM();
+    }
+
+    private void initViewM() {
+        correo=findViewById(R.id.editTextCorreo);
+        nombreUsuario=findViewById(R.id.editTextNombre);
+        mensaje=findViewById(R.id.editTextMensaje);
+        imagen_MSM=findViewById(R.id.imagenMSM);
+        btnEnviar=findViewById(R.id.btnenviar);
     }
 }
